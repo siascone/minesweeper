b36bb58214df391ae6288d0915da23aa03423934
siascone <spencer.iascone@gmail.com>
2020-10-14 17:12:59 -0600

    initial setup of Game component w/ render of Board component

--- components/game.jsx
+++ components/game.jsx
@@ -3,13 +3,22 @@ import * as Minesweeper from '../gameLogic';
 
 class Game extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        const board = new Minesweeper.Board(10, 10);
+        this.state = { board: board };
+
+        this.updateGame = this.updateGame.bind(this)
+
+    }
+
+    updateGame() {
+
     }
 
     render() {
         return (
             <div>
-                This is Minesweeper!!!
+                <Board board={this.state.board} updateGame={this.updateGame} />
             </div>
         )
     }
