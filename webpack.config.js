faa7a94b5d7ce8c22f7bea642b39ad1e6c2acec7
siascone <spencer.iascone@gmail.com>
2020-10-14 16:20:30 -0600

    npm init/install, webpack configuration and gitignore setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: './minesweeper.jsx',
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_moduels)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                }
+            }
+        ]
+    },
+
+    devtool: 'source-map',
+
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+};
