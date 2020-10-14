d6a509f9b380e38303d10bfd0be10a89a7f66a33
siascone <spencer.iascone@gmail.com>
2020-10-14 16:19:02 -0600

    basic set up of game file tree

---
+++ components/game.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                This is Minesweeper!!!
+            </div>
+        )
+    }
+}
+
+export default Game
+
